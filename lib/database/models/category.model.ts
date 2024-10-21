1790a960af6edbf2c2dace08a344bc05c5c9fe06
hernanhuwae <martinushuwae014@gmail.com>
2024-10-21 13:02:51 +0700

    v7

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,14 +1,14 @@
-import { Document, model, models, Schema } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
-export interface ICategory extends Document{
-    _id:String,
-    name_category:string
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
 }
 
-const CategorySchema= new Schema({
-    name_category:{type: String, required:true, unique: true}
+const CategorySchema = new Schema({
+  name: { type: String, required: true, unique: true },
 })
 
-const Category= models.Category || model('Category', CategorySchema)
+const Category = models.Category || model('Category', CategorySchema);
 
-export default Category
+export default Category;
